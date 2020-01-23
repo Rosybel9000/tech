8b1475148ea6bc29738f698dff411e0f35a46576
Rosybel Diaz Santana <rosybel666diaz@gmail.com>
2020-01-23 14:10:34 +0100

    starthub added

---
+++ src/app/start-hub/start-hub.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-hub',
+  templateUrl: './start-hub.component.html',
+  styleUrls: ['./start-hub.component.scss']
+})
+export class StartHubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
