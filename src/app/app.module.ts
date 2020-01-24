06b19a4e2eae48119d5af2bd9a7bd5b1eb7b3c5b
PatrickOorburg <patrick.5000@live.nl>
2020-01-24 16:39:46 +0100

    added opleiding dropdowns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OpleidingenComponent } from './opleidingen/opleidingen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    OpleidingenComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/opleidingen/opleidingen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opleidingen',
+  templateUrl: './opleidingen.component.html',
+  styleUrls: ['./opleidingen.component.scss']
+})
+export class OpleidingenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
